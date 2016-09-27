7479c27e2cef1ccecdc6477026f1e48d0bc8cc94
Niels Kloster Andreassen <nika@telenor.dk>
2016-09-27 21:19:24 +0200

    First implementation of new buttons

--- web/elm/App.elm
+++ web/elm/App.elm
@@ -3,6 +3,7 @@ module FeatureFlax exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.App as App
+import Html.Events exposing (onClick, onSubmit, onInput)
 import List exposing (..)
 import ListExtra exposing (..)
 
@@ -17,10 +18,15 @@ type alias FeatureState =
 type alias Model =
   { featureStates : List FeatureState,
     environments : List Environment,
-    history : List Msg
+    history : List Msg,
+    newFeatureText : String,
+    newEnvironmentText : String
   }
 type Msg =
+  UpdateNewFeature String |
+  UpdateNewEnvironment String |
   AddEnvironment Environment |
+  AddFeature Feature |
   TurnFeatureOn (Feature, Environment) |
   TurnFeatureOff (Feature, Environment)
 
@@ -34,18 +40,16 @@ main =
 
 init : (Model, Cmd Msg)
 init =
-  let initialState =
+  let
+    emptyModel = { featureStates = [], environments = [], history = [], newFeatureText = "", newEnvironmentText = ""}
+    initialState =
     [ 
       AddEnvironment "Production",
       AddEnvironment "Staging",
       AddEnvironment "Development",
-      TurnFeatureOn("Two Factor Authentication", "Development"),
-      TurnFeatureOn("Two Factor Authentication", "Staging"),
-      TurnFeatureOn("Two Factor Authentication", "Production"),
-      TurnFeatureOff("Two Factor Authentication", "Staging"),
-      TurnFeatureOff("Two Factor Authentication", "Development")
+      TurnFeatureOn("Two Factor Authentication", "Development")
     ]
-    |> foldl rollupUpdate { featureStates = [], environments = [], history = []}
+    |> foldl rollupUpdate emptyModel
   in
   (initialState, Cmd.none)
 
@@ -59,11 +63,16 @@ except feature states =
   states |> filter (\ x -> x.name /= feature)
 
 update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-  ({ environments = getEnvironments msg model.environments, 
-     featureStates = getFeatureStates msg model.featureStates,
-     history = msg :: model.history
-  }, Cmd.none)
+update msg modelWithoutHistory =
+  let model = { modelWithoutHistory | history = msg :: modelWithoutHistory.history } in
+  case msg of
+    UpdateNewFeature text -> ({ model | newFeatureText = text }, Cmd.none)
+    UpdateNewEnvironment text -> ({ model | newEnvironmentText = text }, Cmd.none)
+    _ ->
+      ({ model |
+         environments = getEnvironments msg model.environments, 
+         featureStates = getFeatureStates msg model.featureStates
+      }, Cmd.none)
 
 getEnvironments msg environments =
   case msg of
@@ -72,6 +81,7 @@ getEnvironments msg environments =
 
 getFeatureStates msg featureStates =
   case msg of
+  AddFeature feature -> { name = feature, isOnFor = [] } :: featureStates
   TurnFeatureOn (feature, environment) ->
     case featureStates |> find (\ x -> x.name == feature) of
     Nothing ->
@@ -100,16 +110,34 @@ view model =
       drawHeader environments :: 
       (model.featureStates |> map (drawFeature environments))
     ),
+    Html.form [ class "input-group col-lg-4", onSubmit (AddEnvironment model.newEnvironmentText)] [
+      input [type' "text", class "form-control", placeholder "Add environment", onInput UpdateNewEnvironment] [],
+      span [class "input-group-btn"] [ button [class "btn btn-default"] [ text "Add"]]
+    ],
+    Html.form [ class "input-group col-lg-4", onSubmit (AddFeature model.newFeatureText)] [
+      input [type' "text", class "form-control", placeholder "Add feature", onInput UpdateNewFeature] [],
+      span [class "input-group-btn"] [ button [class "btn btn-default"] [ text "Add"]]
+    ],
     ul [ class "history"] (
-      (model.history |> map (\ item -> li [] [ getHistoryText item ]))
+      (model.history |> filter isInHistory |> map (\ item -> li [ ] [ getHistoryText item ]))
     )
   ]
+
+isInHistory msg =
+  case msg of 
+  AddEnvironment _ -> True
+  AddFeature _ -> True
+  TurnFeatureOn _ -> True
+  TurnFeatureOff _ -> True
+  _ -> False
 
 getHistoryText msg =
   case msg of
   AddEnvironment environment -> text ("Added " ++ environment)
+  AddFeature feature -> text ("Added " ++ feature)
   TurnFeatureOn (feature, environment) -> text ("Turned on " ++ feature ++ " for " ++ environment)
   TurnFeatureOff (feature, environment) -> text ("Turned off " ++ feature ++ " for " ++ environment)
+  _ -> text ""
 
 drawHeader environments =
   thead [] ( "" :: environments |> map (\ header -> th [] [ text header ]) )
@@ -118,9 +146,12 @@ drawFeature environments feature =
   tr [ ] ( td [] [text feature.name] :: (environments |> map (drawSlider feature)))
 
 drawSlider feature environment =
-  let isChecked = feature.isOnFor |> any (\ x -> x == environment) in
+  let 
+    isChecked = feature.isOnFor |> any (\ x -> x == environment)
+    toggle = if isChecked then TurnFeatureOff(feature.name, environment) else TurnFeatureOn(feature.name, environment)
+  in
   td [] [ label [ class "switch"] [ 
-    input [ type' "checkbox", checked isChecked ] [],
+    input [ type' "checkbox", checked isChecked, onClick toggle] [],
     div [ class "slider" ] []
   ]]
 
